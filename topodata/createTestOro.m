412812d4287e2369b4c0b0d78369cb092cfbe083
JERidenour <fireflightning@gmail.com>
2017-02-18 17:23:08 +0100

    created test file for testing surface orography

---
+++ topodata/createTestOro.m
@@ -0,0 +1,32 @@
+lonlim = [19.5834 20.000];
+latlim = [66.7459 67.1625];
+[Z refvec] = gtopo30('gtopo30/W020N90', 1, latlim, lonlim); %50 sqm around Harsprånget
+%figure
+%contourf(A) %see the entire dem tile
+Z = Z./1000; %scale data to km above mean sea-level
+%figure
+%contourf(Z) %show a contour plot of the area
+[nx ny] = size(Z);
+
+M = zeros(nx*ny, 3);
+
+
+
+col1 = zeros(nx*ny, 1);
+k=1;
+for i=0:(nx-1)
+    for j=1:ny
+        
+        col1(k) = i;    
+        k = k+1;
+
+    end
+end
+
+col2 = repmat([0:1:(nx-1)],1,ny)';
+
+col3 = reshape(Z, nx*ny, 1);
+
+M = [col1';col2';col3']';
+
+dlmwrite('OROdat.txt', M, '\t')
